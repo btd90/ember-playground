1b676c3b7b908a83205b54629a3e3b909f3b2da2
Ben Davey <bentdavey@gmail.com>
2020-06-23 16:18:05 +1000

    Component now rendering and scalling correctly when zooming/panning on map

--- app/pods/components/map-component/display-zone/component.js
+++ app/pods/components/map-component/display-zone/component.js
@@ -1,91 +1,137 @@
-// import MapComponent from '../component';
 import Component from '@ember/component';
-import {
-  observer,
-  computed,
-} from '@ember/object';
 
 /**
  * EXPECT LAT/LNG/Bounds???
  * Create new L.control????
  */
-// export default MapComponent.extend({
 export default Component.extend({
   init() {
     this._super(...arguments);
 
-    // Update control 
+    //
+    // Update layer-control at some point!!! 
+    //
+
+    // Store inputs
     let map = this.get('mapInstance');
     let layerName = this.get('layer');
     let absolutePosition = this.get('absolutePosition');
     let position = [this.get('lat'), this.get('lng')];
+    let maintainComponentSize = this.get('maintainComponentSize');
 
-    // create new control
-    // let control = L.control();
-
-
-    // Using custom layer
-    //
-    // TEST IT HERE!!
-    //
-
-    // Using control
-    // L.Control.Watermark = L.Control.extend({
-    //   onAdd: function (map) {
-    //     let img = L.DomUtil.create('img');
-
-    //     img.src = 'assets/images/building.png';
-    //     img.style.width = '50px';
-
-    //     return img;
-    //   },
-
-    //   onRemove: function (map) {
-    //     // Nothing to do here
-    //   }
-    // });
-
-    // L.control.watermark = function (opts) {
-    //   return new L.Control.Watermark(opts);
-    // }
-
-    // L.control.watermark({
-    //   position: 'bottomleft'
-    // }).addTo(map);
-
+    // Create new custom map layer
+    // TRY SHIFTING THIS TO OUTSIDE COMPONENT DEFINITION
     L.CustomLayer = L.Layer.extend({
       onAdd: function (map) {
-          let pane = map.getPane(this.options.pane);
-          this._container = L.DomUtil.create('div');
-          this._container.className = layerName;
-          this._container.id = layerName;
+        let pane = map.getPane(this.options.pane);
+        this._container = L.DomUtil.create('div');
+        this._container.className = layerName;
+        this._container.id = layerName;
 
-          pane.appendChild(this._container);
-    
-          // Calculate initial position of container with `L.Map.latLngToLayerPoint()`, `getPixelOrigin()` and/or `getPixelBounds()`
-          let mapPosition = map.latLngToLayerPoint(position);
+        // Required to scale correctly on zoom
+        this._container.style.position = "absolute";
+
+        // Add container to pane
+        pane.appendChild(this._container);
+
+        // this._disableLeafletRounding();
+
+        // Try placing a marker and using that as position???
+
+
+        // Calculate initial position of container with `L.Map.latLngToLayerPoint()`, `getPixelOrigin()` and/or `getPixelBounds()`
+        let mapPosition = map.latLngToLayerPoint(position);
+        // let mapPosition = map.project(position, map._zoom);
+        // debugger;
+
+        // let test =  {x: 26.333333333333258, y: 35.4770169430887};
+
+        // let zoom = this._map._zoom;
+        // let layerOrigin = this._map.layerPointToLatLng([0, 0]);
+        // let projectedPoint = this._map.project(L.latLng(position), zoom);
+        // let projectedOrigin = this._map.project(layerOrigin, zoom);
+        // let mapPosition = projectedPoint._subtract(projectedOrigin);
+
+        // this._enableLeafletRounding();
+
+        if (maintainComponentSize) {
           L.DomUtil.setPosition(this._container, mapPosition);
-    
-          // Add and position children elements if needed
-    
-          // CHANGE ABSOLUTE POSITION TO INSTEAD DEAL WITH RESIZING DIV TO MAINTAIN DIV BOUNDS ON ZOOM!!
-          if (absolutePosition) map.on('zoom viewreset', this._update, this, map);
+        } else {
+          // TRY ADDING MULTIPLIER TO ZOOM??
+          L.DomUtil.setTransform(
+            this._container, 
+            mapPosition, 
+            this._map._zoom
+          );
+        }
+        
+        // Add and position children elements if needed
+
+        // CHANGE ABSOLUTE POSITION TO INSTEAD DEAL WITH RESIZING DIV TO MAINTAIN DIV BOUNDS ON ZOOM!!
+        map.on('zoomend viewreset', this._update, this, map);
       },
-    
-      onRemove: function(map) {
-          L.DomUtil.remove(this._container);
-          if (absolutePosition) map.off('zoom viewreset', this._update, this, map);
+
+      onRemove: function (map) {
+        L.DomUtil.remove(this._container);
+        map.off('zoomend viewreset', this._update, this, map);
       },
-    
-      _update: function() {
-          // Recalculate position of container
-          let mapPosition = this._map.latLngToLayerPoint(position);
-          L.DomUtil.setPosition(this._container, mapPosition);   
-  
-          // Add/remove/reposition children elements if needed
-      }
+
+      _update: function () {
+        // this._disableLeafletRounding();
+        // let test =  {x: 26.333333333333258, y: 35.4770169430887};
+
+        // Recalculate position of container
+        let mapPosition = this._map.latLngToLayerPoint(position);
+        // console.log(position);
+
+        // IS IT WORTH PERSUEING THIS??
+        // REVISIT ONCE EVERYTHING ELSE IS WORKING?
+        // this._enableLeafletRounding();
+
+
+        // let zoom = this._map._zoom;
+        // let layerOrigin = this._map.layerPointToLatLng([0, 0]);
+        // let projectedPoint = this._map.project(L.latLng(position), zoom);
+        // let projectedOrigin = this._map.project(layerOrigin, zoom);
+        // let mapPosition = projectedPoint._subtract(projectedOrigin);
+        // debugger;
+
+        if (maintainComponentSize) {
+          // LAT changes while zooming!!!
+          // SEE COMMENT ABOVE
+          L.DomUtil.setPosition(this._container, mapPosition);
+        } else {
+          // TRY ADDING MULTIPLIER TO ZOOM??
+          L.DomUtil.setTransform(
+            this._container, 
+            mapPosition, 
+            this._map._zoom
+          );
+        }
+
+
+        // // L.DomUtil.setPosition(this._container, mapPosition);
+        // debugger;
+        // L.DomUtil.setTransform(
+        //   this._container, 
+        //   mapPosition, 
+        //   this._map._zoom
+        // );
+        // debugger;
+
+        // Add/remove/reposition children elements if needed
+      },
+
+      _disableLeafletRounding: function(){
+        this._leaflet_round = L.Point.prototype._round;
+        L.Point.prototype._round = function(){ return this; };
+      },
+
+      _enableLeafletRounding: function(){
+        L.Point.prototype._round = this._leaflet_round;
+      },
     });
-    
+
     L.customlayer = function (args) {
       return new L.CustomLayer(args);
     }
@@ -94,51 +140,10 @@ export default Component.extend({
       layerName: layerName,
       absolutePosition: absolutePosition
     }).addTo(map);
-
-    return L.customlayer;
   },
 
   didInsertElement() {
     this._super(...arguments);
     this.set('layerReady', true);
   },
-
-  // test: computed('map', function () {
-  //     debugger;
-  //     let map = this.get('map');
-  //     return map;
-  // }),
 });
-
-// L.CustomLayer = L.Layer.extend(args, {
-//   onAdd: function (map) {
-//       let pane = map.getPane(this.options.pane);
-//       debugger;
-//       this._container = L.DomUtil.create(layerName);
-
-//       pane.appendChild(this._container);
-
-//       // Calculate initial position of container with `L.Map.latLngToLayerPoint()`, `getPixelOrigin()` and/or `getPixelBounds()`
-
-//       // L.DomUtil.setPosition(this._container, point);
-//       L.DomUtil.setPosition(this._container, [0, 0]);
-
-//       // Add and position children elements if needed
-
-//       if (absolutePosition) map.on('zoomend viewreset', this._update, this);
-//   },
-
-//   onRemove: function(map) {
-//       L.DomUtil.remove(this._container);
-//       if (absolutePosition) map.off('zoomend viewreset', this._update, this);
-//   },
-
-//   _update: function() {
-//       // Recalculate position of container
-//       debugger;
-
-//       L.DomUtil.setPosition(this._container, point);        
-
-//       // Add/remove/reposition children elements if needed
-//   }
-// });
